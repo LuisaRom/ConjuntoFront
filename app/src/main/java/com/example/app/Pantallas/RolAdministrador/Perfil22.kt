f4cb540ded63933095b53cfe09b05558fbc78f4e
LuisaRom <lufromero13@gmail.com>
2026-04-28 17:30:22 -0500

    cambiosycambios

--- app/src/main/java/com/example/app/Pantallas/RolAdministrador/Perfil22.kt
+++ app/src/main/java/com/example/app/Pantallas/RolAdministrador/Perfil22.kt
@@ -1,8 +1,10 @@
 package com.example.app.Pantallas.RolAdministrador
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
